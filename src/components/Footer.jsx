4bc3f78c93bd55570bfed909a8766a9f24ecb2c8
Tyson <tysonpederson95@gmail.com>
2024-08-15 08:24:59 -0700

    Footer formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,15 @@ export default function Footer() {
   return (
     <footer>
       <hr className="border-gray-300 border" />
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-gray-500 sm:text-center">
+      <div className="w-full mx-auto max-w-screen-xl py-4 md:flex md:items-center md:justify-between text-gray-500">
+        <span className="sm:text-center">
           © 2024{" "}
           <a href="#" className="hover:underline">
             Tyson Pederson
           </a>
           . All Rights Reserved.
         </span>
-        <p className="text-gray-500 sm:text-center">
+        <p className="sm:text-center">
           Built using <a className="hover:opacity-60 text-sky-500 transition-all duration-200" href="https://react.dev/">React</a> and{" "}
           <a className="hover:opacity-60 text-sky-500 transition-all duration-200" href="https://tailwindcss.com/">Tailwind</a>.
         </p>
